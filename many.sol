0d0a42391aa9695d4bbd93fd201f120318dc4502
PORNX <33781000+PORNX@users.noreply.github.com>
2018-03-08 22:52:24 +0300

    Create many.sol

---
+++ many.sol
@@ -0,0 +1,14 @@
+contract ERC20 {
+  function transfer(address _recipient, uint256 _value) public returns (bool success);
+}
+ 
+contract Airdrop {
+  function transferMany(ERC20 token, address[] recipients, uint256[] values) public {
+    for (uint256 i = 0; i < recipients.length; i++) {
+      token.transfer(recipients[i], values[i]);
+    }
+  }
+  function transfer(ERC20 token, address _address, uint256 _value) public {
+    token.transfer(_address, _value);
+  }
+}
