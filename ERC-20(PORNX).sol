10541019033ed295410430dca72657e5c7cf7e7b
PORNX <33781000+PORNX@users.noreply.github.com>
2018-04-22 04:44:53 +0300

    Update ERC-20(PORNX).sol

--- ERC-20(PORNX).sol
+++ ERC-20(PORNX).sol
@@ -10,7 +10,7 @@ library SafeMath {
   * @dev Multiplies two numbers, throws on overflow.
   */
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {2
+    if (a == 0) {
       return 0;
     }
     uint256 c = a * b;
