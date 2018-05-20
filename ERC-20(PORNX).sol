d7ec96d0218a895ef98b3a71d7d00bbd8a9baaa4
PORNX <33781000+PORNX@users.noreply.github.com>
2018-05-20 12:34:33 +0300

    Update ERC-20(PORNX).sol

--- ERC-20(PORNX).sol
+++ ERC-20(PORNX).sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.18;
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
-library SafeMath {123
+library SafeMath {1234
 
   /**
   * @dev Multiplies two numbers, throws on overflow.
